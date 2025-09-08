3b0113b3056c5415ad5c51be497c0535503377b4
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-08 07:22:02 -0300

    Ya avance algo, pero recien hago el primer commit.

---
+++ app/order/page.tsx
@@ -0,0 +1,8 @@
+export default function OrderPage() {
+  return (
+    <>
+      <h1>ASdas</h1>
+      <p>asd</p>
+    </>
+  );
+}
---
+++ app/products/new/page.tsx
@@ -0,0 +1,3 @@
+export default function NewProductPage() {
+  return <h1>Crear un nuevo producto</h1>;
+}
