ecab5c4aac0a205a538a4fcb129664600d690efc
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-12 12:34:23 -0300

    finalizamos el action de completar la orden

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,3 +1,33 @@
 "use server";
 
-export default async function completeOrder() {}
+import { revalidatePath } from "next/cache";
+import { prisma } from "@/src/lib/prisma";
+import { orderIdSchema } from "@/src/schema";
+
+export default async function completeOrder(formData: FormData) {
+  const data = {
+    orderId: formData.get("order_id")!,
+  };
+  const result = orderIdSchema.safeParse(data);
+
+  if (!result.success) {
+    return;
+  }
+
+  try {
+    await prisma.order.update({
+      where: {
+        id: result.data.orderId,
+      },
+      data: {
+        status: true,
+        orderReadyAt: new Date(Date.now()),
+      },
+    });
+
+    //* Revalidamos la ruta de /admin/orders para que se haga un refetch
+    revalidatePath("/admin/orders");
+  } catch (error) {
+    console.log(error);
+  }
+}
